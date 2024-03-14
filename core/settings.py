eb086f7f9fdefcc750d57da2a8db877b4485bed4
Umidjon12346 <umidshari456@gmail.com>
2024-03-14 15:34:24 +0500

    done

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -50,13 +49,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    
 ]
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-  'http://localhost:8000',
-)
-ROOT_URLCONF = 'core.urls'
+
 
 TEMPLATES = [
     {
